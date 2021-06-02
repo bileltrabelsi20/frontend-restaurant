4ce4508735ce9722aaca7afe632e949f27547a68
bileltrabelsi20 <bileltrabelsi445@gmail.com>
2021-06-02 15:29:04 +0100

    get all service

--- src/app/services/products.service.ts
+++ src/app/services/products.service.ts
@@ -12,39 +12,40 @@ export class ProductsService {
   constructor(  public http : HttpClient) { }
 
   addSandwich(data){
-
     return this.http.post( this.baseURL + '/menu/sandwich',data)
-
-
   }
 
   addBurger(data){
-
     return this.http.post( this.baseURL + '/menu/burger',data)
-
-
   }
 
   addTacos(data){
-
     return this.http.post( this.baseURL + '/menu/tacos',data)
-
-
   }
 
   addIngrediant(data){
-
     return this.http.post( this.baseURL + '/menu/ingrediant',data)
-
-
   }
 
   addImage(data){
-    
     return this.http.post( this.baseURL + '/upload/uploadSingle',data)
-
   }
 
- 
-  
+  //////////////////// get all products ////////////////////////
+
+  getAllSandwich(){
+    return this.http.get( this.baseURL + '/menu/findAllSandwichs')
+  }
+
+  getAllBurger(){
+    return this.http.get( this.baseURL + '/menu/findAllBurgers')
+  }
+
+  getAllTacos(){
+    return this.http.get( this.baseURL + '/menu/findAllTacos')
+  }
+
+  getAllImage(){
+    return this.http.get( this.baseURL + '/upload/uploadSingle')
+  }
 }
