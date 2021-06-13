42279e961ab7bfa68caaf1d96373b7e75a2f015c
bileltrabelsi20 <bileltrabelsi445@gmail.com>
2021-06-13 21:03:28 +0100

    update sandwich

--- src/app/pages/account/products/edit-components/edit-sandwich/edit-sandwich.component.ts
+++ src/app/pages/account/products/edit-components/edit-sandwich/edit-sandwich.component.ts
@@ -1,4 +1,5 @@
 import { Component, Inject, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
 
 @Component({
@@ -8,10 +9,29 @@ import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
 })
 export class EditSandwichComponent implements OnInit {
 
+  sandwichForm: FormGroup;
+  files: File[] = [];
+
+
   constructor(public dialogRef: MatDialogRef<EditSandwichComponent>,
     @Inject(MAT_DIALOG_DATA) public data: any) { }
 
+    onSelect(event) {
+      this.files.push(...event.addedFiles);    
+    }
+  
+    onRemove(event) {
+      this.files.splice(this.files.indexOf(event), 1);
+    }
+
   ngOnInit(): void {
+
+    this.sandwichForm = new FormGroup({
+      nom: new FormControl('', Validators.required),
+      prixPrincipale: new FormControl('', Validators.required),
+      compositions: new FormControl('', Validators.required),
+    })
+
   }
   
   onNoClick(): void {
