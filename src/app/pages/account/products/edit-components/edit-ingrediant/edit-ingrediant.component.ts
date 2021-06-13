255a1e52e2df76146e5d82d4ad2acacc2afc0803
bileltrabelsi20 <bileltrabelsi445@gmail.com>
2021-06-13 21:03:10 +0100

    upadate ingrediant

--- src/app/pages/account/products/edit-components/edit-ingrediant/edit-ingrediant.component.ts
+++ src/app/pages/account/products/edit-components/edit-ingrediant/edit-ingrediant.component.ts
@@ -1,4 +1,5 @@
 import { Component, Inject, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
 
 @Component({
@@ -7,11 +8,28 @@ import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
   styleUrls: ['./edit-ingrediant.component.scss']
 })
 export class EditIngrediantComponent implements OnInit {
+  
+  ingrediantsForm: FormGroup;
+  files: File[] = [];
 
   constructor(public dialogRef: MatDialogRef<EditIngrediantComponent>,
     @Inject(MAT_DIALOG_DATA) public data: any) { }
 
+    onSelect(event) {
+      this.files.push(...event.addedFiles);    
+    }
+  
+    onRemove(event) {
+      this.files.splice(this.files.indexOf(event), 1);
+    }
+
   ngOnInit(): void {
+
+    this.ingrediantsForm = new FormGroup({
+      nomIngrediant: new FormControl('', Validators.required),
+      prixIngrediant: new FormControl('', Validators.required),
+    })
+
   }
   
   onNoClick(): void {
