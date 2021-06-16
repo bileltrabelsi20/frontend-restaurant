ad0c88be0300a10d64faf25d99577a796166397a
bileltrabelsi20 <bileltrabelsi445@gmail.com>
2021-06-16 14:28:27 +0100

    edit burger

--- src/app/pages/account/products/edit-components/edit-burger/edit-burger.component.ts
+++ src/app/pages/account/products/edit-components/edit-burger/edit-burger.component.ts
@@ -1,5 +1,5 @@
 import { Component, Inject, OnInit } from '@angular/core';
-import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { FormControl, FormGroup } from '@angular/forms';
 import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
 
 @Component({
@@ -8,22 +8,51 @@ import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
   styleUrls: ['./edit-burger.component.scss']
 })
 export class EditBurgerComponent implements OnInit {
-  burgerForm : FormGroup;
+  burgerUpdateForm : FormGroup;
+  files: File[] = [];
 
   constructor(public dialogRef: MatDialogRef<EditBurgerComponent>,
     @Inject(MAT_DIALOG_DATA) public data: any) { }
 
+    /////////////////////// add image /////////////////////////
+
+    onSelect(event) {
+      this.files.push(...event.addedFiles);    
+    }
+    onRemove(event) {
+      this.files.splice(this.files.indexOf(event), 1);
+    }
+
+    ///////////////////////////////////////////////////////////
+
   ngOnInit(): void {
-    this.burgerForm = new FormGroup({
-      nom: new FormControl('', Validators.required),
-      prixPrincipale: new FormControl('', Validators.required),
-      compositions: new FormControl('', Validators.required),
+    this.burgerUpdateForm = new FormGroup({
+      nom: new FormControl(this.data.nom),
+      prixPrincipale: new FormControl(this.data.prixPrincipale),
+      compositions: new FormControl(this.data.compositions),
     })
   }
   
   onNoClick(): void {
     this.dialogRef.close();
   }
+
+  updateBurger(){
+
+    const newFormData = new FormData();
+    // newFormData.set('nom',this.tacosUpdateForm.get('nom').value);
+
+    // form data : boucle for pour les keys : nom , prixPrincipale , compositions
+    Object.keys(this.burgerUpdateForm.value).forEach(key=>{
+      newFormData.set(key,this.burgerUpdateForm.get(key).value);
+    });
+
+    // ajout image after update , this.files[0] : nsobou a partier de l'indice 0 :
+    newFormData.append('imageBurger', this.files[0]);
+    // on ferme le dialog apres l'update :
+    this.dialogRef.close(newFormData);
+
+    }
 
 
 }
